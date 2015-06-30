58af2b1dad1b43880e0a0dde84858e1b0926cf84
unknown <in8rakma@DKV1518.corp.lego.com>
2015-06-30 16:41:23 +0530

    :

---
+++ src/IE/IEBrowser.java
@@ -0,0 +1,27 @@
+package IE;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public class IEBrowser {
+	public static  WebDriver driver=null;
+	@Test
+	
+	public void launchIE()
+	{
+		System.setProperty("webdriver.ie.driver", System.getProperty("user.dir")+"\\InternetExplorerDriver\\IEDriverServer.exe");
+		
+		
+		DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+		caps.setCapability(
+		    InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,
+		    true);
+		WebDriver driver = new InternetExplorerDriver(caps);
+		
+			
+	}
+
+
+}
